685e3b9cdd54cfd665927ecc9e8509c8d5822975
omkarrathod123 <omkarrathod0705@gmail.com>
2025-05-04 16:45:32 +0530

    Creating python Code!!

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# docker-py
+# Docker-py
 In this repository, we are going to see how to create docker file for python project
+
+**Flow**
+1.> write a basic python code
+2.> write Dockerfile
+3.> Run "docker build ."
+4.> Run "docker run {image ID}"
---
+++ index.py
@@ -0,0 +1,10 @@
+form random import randint
+
+min_number = int(input('please enter min: '))
+max_number = int(input('please enter max: '))
+
+if (max_number < min_number):
+    print('Invalid input - Error')
+else:
+    rad_number = randint(min_number,max_number)
+    print(rad_number)
